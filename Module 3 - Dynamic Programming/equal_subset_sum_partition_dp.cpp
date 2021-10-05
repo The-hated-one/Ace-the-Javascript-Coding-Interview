48398c3bab3fdf59f593a3ba73167be948b44e0f
ankur <ankur.practice11@gmail.com>
2021-10-05 22:00:32 +0530

    Updated "Module 3 - Dynamic Programming\equal_subset_sum_partition_dp.cpp"

--- Module 3 - Dynamic Programming/equal_subset_sum_partition_dp.cpp
+++ Module 3 - Dynamic Programming/equal_subset_sum_partition_dp.cpp
@@ -1,4 +1,11 @@
 
+/*
+Problem Statement
+
+Given a set of positive numbers, find if we can partition it into 
+two subsets such that the sum of elements 
+in both subsets is equal.
+*/
 
 #include<bits/stdc++.h>
 using namespace std;
